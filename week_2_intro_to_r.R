0329c57c7b087d7490381df11a545f82505a899e
marcocastanedamartinez <mrcastanedamartinez@ucdavis.edu>
2024-10-03 15:00:37 -0700

    Assignments week 2

--- week_2_intro_to_r.R
+++ week_2_intro_to_r.R
@@ -63,3 +63,64 @@ elephant2_lb<-7757
 elephant2_lb<-elephant1_kg * 2.2
 
 elephant2_lb > elephant1_lb
+
+
+#############################################################
+getwd()
+# change working directory 
+setwd()
+
+
+d<-read.csv("the dolation of your date file")
+
+# create scrpits file ---------
+dir.create("./lectures")
+
+# how r thinks about data---------
+### This is setting a series of values. ------
+
+## Vectors -----
+weight_g<-c(50,60,65,82)
+weight_one_value<-c(50)
+
+animals<-c("mouse","rat","dog")
+animals
+
+### inspection ---------
+length(weight_g)
+str(weight_g)
+
+### change vector--------
+
+weight_g<-c(weight_g,90)
+weight_g
+
+num_char <-c(1,2,3, "a")
+num_char
+num_logical<-c(1,2,3,"TRUE")
+num_logical
+# coerces values to be all the ame, e.g. when TRUE is included. 
+char_logical<-c("a","b","c", TRUE)
+char_logical
+tricky<-c(1,2,3,"4")
+
+####### chooses the lowest common denominator # vectors have to be the SAME class of values. 
+
+
+# subsetting 
+animals<-c("mouse", "rat","dog","cat")
+animals 
+animals[2]
+animals[c(2,3)]
+# give me some combination or subset of the original document 
+##### conditional sub-setting 
+weight_g<c(21,34,39,54,55)
+weight_g>50
+weight_g[weight_g>50]
+
+## additional symbols you can use in the subset 
+# I want to grab these values from another set of values %in%
+animals %in% c("rat","cat","dog","duck","goat")
+animals == c("rat","cat","dog","duck","goat")
+# == has to be matched pairwise 
+
