bad38b2d1b590ff5ca2e0f035eeb26c42fd5dfac
marcocastanedamartinez <mrcastanedamartinez@ucdavis.edu>
2024-12-09 12:48:26 -0800

    Redid the homework so the graph can work.

--- week_8_homework.R
+++ week_8_homework.R
@@ -2,15 +2,24 @@ library(tidyverse)
 library(ggplot)
 mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
 
+mloa2 <- mloa %>% 
+  # Filter invalid data values
+  filter(rel_humid != -99, temp_C_2m != -999.9, windSpeed_m_s != -999.9) %>% 
+  # Create datetime and convert to local time zone
+  mutate(datetime = make_datetime(year = year, month = month, day = day, hour = hour24, min = min),
+         datetimeLocal = with_tz(datetime, tz = "Pacific/Honolulu"),
+         localMon = month(datetimeLocal, label = TRUE),
+         localHour = hour(datetimeLocal)) 
 
- mloaplot<- mloa %>% 
-   filter(rel_humid !=-99 & temp_C_2m !=-999.9 & windSpeed_m_s != -999.9) %>%
-   mutate(datetime = ymd_hm(paste0(year, "-", month, "-", day, " ", hour24, ":", min), tz = "UTC")) %>% 
-   mutate(datetimeLocal = with_tz(datetime, tz = "Pacific/Honolulu"),
-          localMon = month(datetimeLocal, label = TRUE), localHour = hour(datetimeLocal)) %>% 
-   group_by(localMon, localHour) %>% summarize(mean(temp_C_2m)) %>% 
-   ggplot(aes(x=localMon, y= meantemp))+ geom(aes(col=localHour))+
-   scale_color_continuous()+ggtitle("Mean Temperature as a Function of time- Mloa")+xlab("Month")+
-   ylab(expression(" Mean Temperature ("*~degree*C*")"))+theme_minimal()
-              
+# Summarize and plot
+mloa_summary <- mloa2 %>% 
+  group_by(localMon, localHour) %>% 
+  summarize(meantemp = mean(temp_C_2m, na.rm = TRUE), .groups = 'drop')
+
+ggplot(mloa_summary, aes(x = localMon, y = meantemp)) + 
+  geom_point(aes(col = localHour)) + 
+  scale_color_continuous() + 
+  xlab("Month") + 
+  ylab("Mean Temperature (°C)") + 
+  theme_classic()
  #### okay, this one took forever 
